d0d0a083e46c21ee23f35220460abf66bb855d82
Ørjan Kløvfjell Thoresen <orjan-thoresen@hotmail.com>
2022-02-23 21:38:43 +0100

    Updated UNET models

--- Models/unet.py
+++ Models/unet.py
@@ -3,7 +3,7 @@ from keras.layers import Input, Conv2D, Dense, Flatten, Reshape, Conv2DTranspose
 from keras.layers import MaxPooling2D, UpSampling2D, Dropout, concatenate
 
 
-def unet_org(inputs):
+def unet_w_dropout(inputs, dropout_rate: float = 0.35):
     # inputs = Input(shape=input_size)
 
     c1 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(inputs)
@@ -26,10 +26,12 @@ def unet_org(inputs):
     p4 = MaxPooling2D(pool_size=(2, 2))(c4skip)
 
     c5 = Conv2D(512, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p4)
-    c5 = Conv2D(1024, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c5)
-    bottle = Conv2D(1024, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c5)
+    drop4 = Dropout(dropout_rate)(c5)
+    bottle = Conv2D(1024, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(drop4)
+    c5 = Conv2D(1024, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(bottle)
+    drop5 = Dropout(dropout_rate)(c5)
 
-    u6 = Conv2DTranspose(512, (2, 2), strides=(2, 2), padding='same')(bottle)
+    u6 = Conv2DTranspose(512, (2, 2), strides=(2, 2), padding='same')(drop5)
     u6 = concatenate([u6, c4skip])
     c6 = Conv2D(512, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u6)
     c6 = Conv2D(512, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c6)
@@ -54,7 +56,65 @@ def unet_org(inputs):
     return output, bottle
 
 
-def new_unet_org_dense(inputs, dropout_rate: float = 0.35):
+def unet_org_dense(inputs, dropout_rate: float = 0.35):
+    # inputs = Input(shape=input_size)
+
+    c1 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(inputs)
+    c1skip = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c1)
+    p1 = MaxPooling2D((2, 2))(c1skip)
+
+    c2 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p1)
+    c2 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c2)
+    c2skip = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c2)
+    p2 = MaxPooling2D((2, 2))(c2skip)
+
+    c3 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p2)
+    c3 = Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c3)
+    c3skip = Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c3)
+    p3 = MaxPooling2D((2, 2))(c3skip)
+
+    c4 = Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p3)
+    c4 = Conv2D(512, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c4)
+    c4skip = Conv2D(512, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c4)
+    drop4 = Dropout(dropout_rate)(c4skip)
+    p4 = MaxPooling2D(pool_size=(2, 2))(drop4)
+
+    c5 = Conv2D(512, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p4)
+    c5 = Conv2D(1024, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c5)
+    flatten = Flatten()(c5)
+
+    d1 = Dense(576, activation="relu")(flatten)
+    bottle = Dense(120, activation='sigmoid')(d1)
+    d2 = Dense(576, activation='relu')(bottle)
+    reshape = Reshape((24, 24, 1))(d2)
+
+    c5 = Conv2D(1024, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(reshape)
+
+    u6 = Conv2DTranspose(512, (2, 2), strides=(2, 2), padding='same')(c5)
+    u6 = concatenate([u6, c4skip])
+    c6 = Conv2D(512, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u6)
+    c6 = Conv2D(512, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c6)
+
+    u7 = Conv2DTranspose(256, (2, 2), strides=(2, 2), padding='same')(c6)
+    u7 = concatenate([u7, c3skip])
+    c7 = Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u7)
+    c7 = Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c7)
+
+    u8 = Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same')(c7)
+    u8 = concatenate([u8, c2skip])
+    c8 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u8)
+    c8 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c8)
+
+    u9 = Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same')(c8)
+    u9 = concatenate([u9, c1skip])
+    c9 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u9)
+    c9 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c9)
+
+    output = Conv2D(1, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c9)
+
+    return output, bottle
+
+def unet_org_dense_dropout(inputs, dropout_rate: float = 0.35):
     # inputs = Input(shape=input_size)
 
     c1 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(inputs)
@@ -85,15 +145,15 @@ def new_unet_org_dense(inputs, dropout_rate: float = 0.35):
     c5 = Conv2D(1024, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c5)
     flatten = Flatten()(c5)
 
-    mod = Dropout(dropout_rate)(flatten)
-    bottle = Dense(120, activation='sigmoid')(mod)
-    d2 = Dense(1152, activation='relu')(bottle)
-    reshape = Reshape((24, 24, 2))(d2)
+    d1 = Dense(576, activation="relu")(flatten)
+    dropout = Dropout(dropout_rate)(d1)
+    bottle = Dense(120, activation='sigmoid')(dropout)
+    d2 = Dense(576, activation='relu')(bottle)
+    reshape = Reshape((24, 24, 1))(d2)
 
     c5 = Conv2D(1024, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(reshape)
-    drop5 = Dropout(dropout_rate)(c5)
 
-    u6 = Conv2DTranspose(512, (2, 2), strides=(2, 2), padding='same')(drop5)
+    u6 = Conv2DTranspose(512, (2, 2), strides=(2, 2), padding='same')(c5)
     u6 = concatenate([u6, c4skip])
     c6 = Conv2D(512, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u6)
     c6 = Conv2D(512, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c6)
@@ -117,8 +177,9 @@ def new_unet_org_dense(inputs, dropout_rate: float = 0.35):
 
     return output, bottle
 
+def unet(inputs, dropout_rate: float = 0.35):
+    # inputs = Input(shape=input_size)
 
-def unet_org_dense(inputs, dropout_rate: float = 0.5, skip_connections: bool = True):
     c1 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(inputs)
     c1skip = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c1)
     p1 = MaxPooling2D((2, 2))(c1skip)
@@ -138,14 +199,11 @@ def unet_org_dense(inputs, dropout_rate: float = 0.5, skip_connections: bool = T
     c4skip = Conv2D(512, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c4)
     p4 = MaxPooling2D(pool_size=(2, 2))(c4skip)
 
-    flatten = Flatten()(p4)
-    d1 = Dense(1152, activation='relu')(flatten)
-    # mod = Dropout(dropout_rate)(d1)
-    bottle = Dense(120, activation='sigmoid')(d1)
-    d2 = Dense(1152, activation='relu')(bottle)
-    reshape = Reshape((24, 24, 2))(d2)
+    c5 = Conv2D(512, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p4)
+    c5 = Conv2D(1024, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c5)
+    bottle = Conv2D(1024, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c5)
 
-    u6 = Conv2DTranspose(512, (2, 2), strides=(2, 2), padding='same')(reshape)
+    u6 = Conv2DTranspose(512, (2, 2), strides=(2, 2), padding='same')(bottle)
     u6 = concatenate([u6, c4skip])
     c6 = Conv2D(512, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u6)
     c6 = Conv2D(512, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c6)
