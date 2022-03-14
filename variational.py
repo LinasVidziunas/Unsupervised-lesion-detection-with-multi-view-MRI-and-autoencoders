4304dab6050dec56391cc3ffe20243a454da75c1
Linas Vidziunas <linasvidz@gmail.com>
2022-03-14 16:53:58 +0100

    Dense pre and post bottleneck

--- variational.py
+++ variational.py
@@ -50,7 +50,7 @@ class VAE_UNET(Model):
 
 
     def _model(self, inputs,
-              encoder_filters=[64, 64, 128, 128, 256, 256, 512, 512, 1024, 1024],
+              encoder_filters=[64, 64, 128, 128, 256, 256, 512, 512, 1024, 128],
               decoder_filters=[512, 512, 512, 256, 256, 256, 128, 128, 128, 64, 64, 64]
               ):
         c1 = Conv2D(encoder_filters[0], (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(inputs)
@@ -74,32 +74,37 @@ class VAE_UNET(Model):
 
         flatten = Flatten()(c5)
 
+        # Change name
+        flatten = Dense(3000)(flatten)
+
         z_mean = Dense(self.latent_dim, name="z_mean")(flatten)
         z_log_var = Dense(self.latent_dim, name="z_log_var")(flatten)
         z = Sampling()([z_mean, z_log_var])
 
-        reshape = Reshape((24, 24, 1))(z)
-        
+        z = Dense(73728)(z)
+
+        reshape = Reshape((24, 24, 128))(z)
+
         u6 = Conv2DTranspose(decoder_filters[0], (2, 2), strides=(2, 2), padding='same')(reshape)
         u6 = concatenate([u6, c4skip])
         c6 = Conv2D(decoder_filters[1], (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u6)
         c6 = Conv2D(decoder_filters[2], (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c6)
-        
+
         u7 = Conv2DTranspose(decoder_filters[3], (2, 2), strides=(2, 2), padding='same')(c6)
         u7 = concatenate([u7, c3skip])
         c7 = Conv2D(decoder_filters[4], (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u7)
         c7 = Conv2D(decoder_filters[5], (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c7)
-        
+
         u8 = Conv2DTranspose(decoder_filters[6], (2, 2), strides=(2, 2), padding='same')(c7)
         u8 = concatenate([u8, c2skip])
         c8 = Conv2D(decoder_filters[7], (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u8)
         c8 = Conv2D(decoder_filters[8], (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c8)
-        
+
         u9 = Conv2DTranspose(decoder_filters[9], (2, 2), strides=(2, 2), padding='same')(c8)
         u9 = concatenate([u9, c1skip])
         c9 = Conv2D(decoder_filters[10], (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u9)
         c9 = Conv2D(decoder_filters[11], (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c9)
-        
+
         output = Conv2D(1, (3, 3), activation='sigmoid', kernel_initializer='he_normal', padding='same')(c9)
         return output, z_mean, z_log_var, z
 
